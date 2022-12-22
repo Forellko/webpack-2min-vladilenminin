e03a1bd54ba3f35c0efac8a6f18c3e785fd5e39f
forellko <dragonforce97q@gmail.com>
2022-12-22 13:26:13 +0300

    add path.resolve and clean:true

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 // NodeJS Code Here!
+const path = require('path');
 
 module.exports = {
   entry: './index.js',
   output: {
     filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 };
